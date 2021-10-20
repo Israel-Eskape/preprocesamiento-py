085c6c95bc56fa9678533bea5c5efe7894185a03
Israel-Eskape <isra.ixoye@gmail.com>
2021-10-20 14:29:00 -0500

    remove and replace datos faltantes

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -1,6 +1,21 @@
+from os import replace
 import pandas as pd 
+import numpy as np
+
 df = pd.read_csv('train.csv')
-#print(df.head()) #muestra todos los datos
-print(df.tail())  #muestra las últimas n filas
+alldata = df.head() #muestra todos los datos
+rows = df.tail()  #muestra las últimas n filas
+datatypes = df.dtypes #muestra los tipos de datos de las columnas
+describedata = df.describe(include="all") #Analisis estadístico de los datos
 
+rowRemove = df.dropna(axis=0)    #dropna elimina datos faltantes
+                            # axis = 0 elimina fila and axis = 1 elimina columnas 
+#Elimina datos en una columna especifica
+removeRowName = df.dropna(subset=["Cabin"], axis = 0)
 
+promedio = df["Age"].mean()
+promedio = round(promedio, 0)
+
+replaceAge = df["Age"].replace(np.nan,promedio)
+replaceAge = replaceAge.head(50)
+print(replaceAge)
