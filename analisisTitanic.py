7de6d530459324ca2d62f258085ca4e2384d2f78
Israel-Eskape <isra.ixoye@gmail.com>
2021-10-26 12:04:14 -0500

    Se imputa la mediana en la edad para no tener valores nulos

--- analisisTitanic.py
+++ analisisTitanic.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 #Carga de datos
 train = pd.read_csv('train.csv')
 #print (train.head())
@@ -19,6 +20,30 @@ survived = train.groupby(['Survived']).count()['PassengerId']
 #target vs sex
 survivedSex = train.groupby(['Survived','Sex']).count()['PassengerId']
 (survivedSex.unstack(level=0).plot.bar())
-plt.show()
 
-print (survivedSex)
+survivedAge = train.groupby(['Sex','Age']).count()['PassengerId']
+(survivedAge.unstack(level=0).plot.bar())
+
+classEmbarked = train.groupby(['Pclass','Embarked']).count()['PassengerId']
+(classEmbarked.unstack(level=0).plot.bar())
+
+#plt.show()
+#print (survivedAge)
+
+
+#procesamiento de datos
+#variables Survived Sex Age Pclass
+
+procesData = train[['Survived', 'Sex', 'Age', 'Pclass']].head(3)
+#procesDataInfo = train[['Survived', 'Sex', 'Age', 'Pclass']].info() #estudiar variables nulos
+train[['Survived', 'Sex', 'Age', 'Pclass']].info()
+datosNulosEdad = (train[train['Age'].isna()].groupby(['Sex', 'Pclass']).count()['PassengerId'].unstack(level = 0))
+
+datosAge = (train[train['Age'].isna()].groupby(['SibSp','Parch']).count()['PassengerId'].unstack(level=0))
+#calcular la media de la edad
+mediaAge = (train['Age'].median())
+train['Age'] = train['Age'].fillna(mediaAge)
+train[['Survived', 'Sex', 'Age', 'Pclass']].info()
+
+
+print(mediaAge)
